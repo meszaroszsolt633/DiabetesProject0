5de3921525e3882e8ac7b4ee03aa2f90a1c1d483
meszaroszsolt633 <meszaroszsolt633@gmail.com>
2022-10-28 13:43:39 +0200

    Separated cleaned xml files into multiple directories

--- defines.py
+++ defines.py
@@ -6,6 +6,10 @@ TEST_DIR = os.path.join(ROOT_DIR, 'Test')
 TEST2_DIR = os.path.join(ROOT_DIR, 'Test2')
 TRAIN_DIR = os.path.join(ROOT_DIR, 'Train')
 TRAIN2_DIR = os.path.join(ROOT_DIR, 'Train2')
+CLEANEDTEST_DIR = os.path.join(ROOT_DIR, 'CleanedTest')
+CLEANEDTEST2_DIR = os.path.join(ROOT_DIR, 'CleanedTest2')
+CLEANEDTRAIN_DIR = os.path.join(ROOT_DIR, 'CleanedTrain')
+CLEANEDTRAIN2_DIR = os.path.join(ROOT_DIR, 'CleanedTrain2')
 CLEANED_DATA_DIR=os.path.join(ROOT_DIR,'Cleaned Data')
 CLEANED_DATA_DIR2=os.path.join(ROOT_DIR,'Cleaned Data2')
 
@@ -24,30 +28,64 @@ TRAIN2_552_PATH = os.path.join(TRAIN2_DIR, '552-ws-training.xml')
 TRAIN2_567_PATH = os.path.join(TRAIN2_DIR, '567-ws-training.xml')
 TRAIN2_596_PATH = os.path.join(TRAIN2_DIR, '596-ws-training.xml')
 
+CLEANEDTRAIN_559_PATH = os.path.join(TRAIN_DIR, '559-ws-trainingCleanedxml')
+CLEANEDTRAIN_563_PATH = os.path.join(TRAIN_DIR, '563-ws-trainingCleaned.xml')
+CLEANEDTRAIN_570_PATH = os.path.join(TRAIN_DIR, '570-ws-trainingCleaned.xml')
+CLEANEDTRAIN_575_PATH = os.path.join(TRAIN_DIR, '575-ws-trainingCleaned.xml')
+CLEANEDTRAIN_588_PATH = os.path.join(TRAIN_DIR, '588-ws-trainingCleaned.xml')
+CLEANEDTRAIN_591_PATH = os.path.join(TRAIN_DIR, '591-ws-trainingCleaned.xml')
+
+CLEANEDTRAIN2_540_PATH = os.path.join(TRAIN2_DIR, '540-ws-trainingCleaned.xml')
+CLEANEDTRAIN2_544_PATH = os.path.join(TRAIN2_DIR, '544-ws-trainingCleaned.xml')
+CLEANEDTRAIN2_552_PATH = os.path.join(TRAIN2_DIR, '552-ws-trainingCleaned.xml')
+CLEANEDTRAIN2_567_PATH = os.path.join(TRAIN2_DIR, '567-ws-trainingCleaned.xml')
+CLEANEDTRAIN2_596_PATH = os.path.join(TRAIN2_DIR, '596-ws-trainingCleaned.xml')
+
 TRAIN_FILE_PATHS = [TRAIN_559_PATH, TRAIN_563_PATH, TRAIN_570_PATH, TRAIN_575_PATH, TRAIN_588_PATH, TRAIN_591_PATH]
 TRAIN2_FILE_PATHS = [TRAIN2_540_PATH, TRAIN2_544_PATH, TRAIN2_552_PATH, TRAIN2_567_PATH, TRAIN2_596_PATH]
+CLEANEDTRAIN_FILE_PATHS = [CLEANEDTRAIN_559_PATH, CLEANEDTRAIN_563_PATH, CLEANEDTRAIN_570_PATH, CLEANEDTRAIN_575_PATH, CLEANEDTRAIN_588_PATH, CLEANEDTRAIN_591_PATH]
+CLEANEDTRAIN2_FILE_PATHS = [CLEANEDTRAIN2_540_PATH, CLEANEDTRAIN2_544_PATH, CLEANEDTRAIN2_552_PATH, CLEANEDTRAIN2_567_PATH, CLEANEDTRAIN2_596_PATH]
 ALL_TRAIN_FILE_PATHS = TRAIN_FILE_PATHS + TRAIN2_FILE_PATHS
+CLEANEDALL_TRAIN_FILE_PATHS = CLEANEDTRAIN_FILE_PATHS + CLEANEDTRAIN2_FILE_PATHS
 
 
-TEST_559_PATH = os.path.join(TEST_DIR, '559-ws-testing.xml')
-TEST_563_PATH = os.path.join(TEST_DIR, '563-ws-testing.xml')
-TEST_570_PATH = os.path.join(TEST_DIR, '570-ws-testing.xml')
-TEST_575_PATH = os.path.join(TEST_DIR, '575-ws-testing.xml')
-TEST_588_PATH = os.path.join(TEST_DIR, '588-ws-testing.xml')
-TEST_591_PATH = os.path.join(TEST_DIR, '591-ws-testing.xml')
+TEST_559_PATH = os.path.join(TEST_DIR, '559-ws-testingCleaned.xml')
+TEST_563_PATH = os.path.join(TEST_DIR, '563-ws-testingCleaned.xml')
+TEST_570_PATH = os.path.join(TEST_DIR, '570-ws-testingCleaned.xml')
+TEST_575_PATH = os.path.join(TEST_DIR, '575-ws-testingCleaned.xml')
+TEST_588_PATH = os.path.join(TEST_DIR, '588-ws-testingCleaned.xml')
+TEST_591_PATH = os.path.join(TEST_DIR, '591-ws-testingCleaned.xml')
 
-TEST2_540_PATH = os.path.join(TEST2_DIR, '540-ws-testing.xml')
-TEST2_544_PATH = os.path.join(TEST2_DIR, '544-ws-testing.xml')
-TEST2_552_PATH = os.path.join(TEST2_DIR, '552-ws-testing.xml')
-TEST2_567_PATH = os.path.join(TEST2_DIR, '567-ws-testing.xml')
-TEST2_584_PATH = os.path.join(TEST2_DIR, '584-ws-testing.xml')
-TEST2_596_PATH = os.path.join(TEST2_DIR, '596-ws-testing.xml')
+TEST2_540_PATH = os.path.join(TEST2_DIR, '540-ws-testingCleaned.xml')
+TEST2_544_PATH = os.path.join(TEST2_DIR, '544-ws-testingCleaned.xml')
+TEST2_552_PATH = os.path.join(TEST2_DIR, '552-ws-testingCleaned.xml')
+TEST2_567_PATH = os.path.join(TEST2_DIR, '567-ws-testingCleaned.xml')
+TEST2_584_PATH = os.path.join(TEST2_DIR, '584-ws-testingCleaned.xml')
+TEST2_596_PATH = os.path.join(TEST2_DIR, '596-ws-testingCleaned.xml')
+
+CLEANEDTEST_559_PATH = os.path.join(TEST_DIR, '559-ws-testingCleaned.xml')
+CLEANEDTEST_563_PATH = os.path.join(TEST_DIR, '563-ws-testingCleaned.xml')
+CLEANEDTEST_570_PATH = os.path.join(TEST_DIR, '570-ws-testingCleaned.xml')
+CLEANEDTEST_575_PATH = os.path.join(TEST_DIR, '575-ws-testingCleaned.xml')
+CLEANEDTEST_588_PATH = os.path.join(TEST_DIR, '588-ws-testingCleaned.xml')
+CLEANEDTEST_591_PATH = os.path.join(TEST_DIR, '591-ws-testingCleaned.xml')
+
+CLEANEDTEST2_540_PATH = os.path.join(TEST2_DIR, '540-ws-testingCleaned.xml')
+CLEANEDTEST2_544_PATH = os.path.join(TEST2_DIR, '544-ws-testingCleaned.xml')
+CLEANEDTEST2_552_PATH = os.path.join(TEST2_DIR, '552-ws-testingCleaned.xml')
+CLEANEDTEST2_567_PATH = os.path.join(TEST2_DIR, '567-ws-testingCleaned.xml')
+CLEANEDTEST2_584_PATH = os.path.join(TEST2_DIR, '584-ws-testingCleaned.xml')
+CLEANEDTEST2_596_PATH = os.path.join(TEST2_DIR, '596-ws-testingCleaned.xml')
 
 TEST_FILE_PATHS = [TEST_559_PATH, TEST_563_PATH, TEST_570_PATH, TEST_575_PATH, TEST_588_PATH, TEST_591_PATH]
 TEST2_FILE_PATHS = [TEST2_540_PATH, TEST2_544_PATH, TEST2_552_PATH, TEST2_567_PATH, TEST2_584_PATH, TEST2_596_PATH]
+CLEANEDTEST_FILE_PATHS = [CLEANEDTEST_559_PATH, CLEANEDTEST_563_PATH, CLEANEDTEST_570_PATH, CLEANEDTEST_575_PATH, CLEANEDTEST_588_PATH, CLEANEDTEST_591_PATH]
+CLEANEDTEST2_FILE_PATHS = [CLEANEDTEST2_540_PATH, CLEANEDTEST2_544_PATH, CLEANEDTEST2_552_PATH, CLEANEDTEST2_567_PATH, CLEANEDTEST2_584_PATH, CLEANEDTEST2_596_PATH]
 ALL_TEST_FILE_PATHS = TEST_FILE_PATHS + TEST2_FILE_PATHS
+CLEANEDALL_TEST_FILE_PATHS = CLEANEDTEST_FILE_PATHS + CLEANEDTEST2_FILE_PATHS
 
 ALL_FILE_PATHS = ALL_TRAIN_FILE_PATHS + ALL_TEST_FILE_PATHS
+CLEANEDALL_FILE_PATHS=CLEANEDALL_TRAIN_FILE_PATHS+CLEANEDALL_TEST_FILE_PATHS
 
 
 ts_tags = [
---
+++ model.py
@@ -0,0 +1,8 @@
+from defines import *
+from functions import *
+from xml_read import *
+from xml_write import *
+import tensorflow as tf
+from tensorflow import keras
+
+#if __name__ == "__main__":
