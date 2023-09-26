97ab86fab3cb4af5da4ed82018f04c4b99720f6e
MarkFallen <erenyi.ate@gmail.com>
2023-09-26 17:30:44 +0200

    multioutput RNN now gives output (terrible)

--- modelMealClassificationRNN.py
+++ modelMealClassificationRNN.py
@@ -107,7 +107,9 @@ def model_base_RNN(dataTrain, dataValidation, lookback=50, maxfiltersize=10, epo
     features_train = pd.concat([feature_train1, feature_train2])
     features_train = features_train.sort_values(by='ts', ignore_index=True)
 
-    features_train_y = [features_train['eaten'],features_train['carbs']]
+    features_train_y = pd.concat([features_train['eaten'], features_train['carbs']], axis=1)
+    features_train_y = features_train_y.to_numpy()
+    features_train_y = features_train_y.astype(float)
     # Apply maximum filter separately to each column
     eaten_max_filtered = ndimage.maximum_filter(features_train_y[:, 0], size=maxfiltersize)
     carbs_max_filtered = ndimage.maximum_filter(features_train_y[:, 1], size=maxfiltersize)
@@ -134,7 +136,10 @@ def model_base_RNN(dataTrain, dataValidation, lookback=50, maxfiltersize=10, epo
     features_validation = pd.concat([feature_validation1, feature_validation2])
     features_validation = features_validation.sort_values(by='ts', ignore_index=True)
 
-    features_validation_y = [features_validation['eaten'],features_validation['carbs']]
+    features_validation_y = pd.concat([features_validation['eaten'], features_validation['carbs']], axis=1)
+    features_validation_y = features_validation_y.to_numpy()
+    features_validation_y = features_validation_y.astype(float)
+    # Apply maximum filter separately to each column
     eaten_max_filtered = ndimage.maximum_filter(features_validation_y[:, 0], size=maxfiltersize)
     carbs_max_filtered = ndimage.maximum_filter(features_validation_y[:, 1], size=maxfiltersize)
 
@@ -233,57 +238,50 @@ def model_meal_RNN_multioutput(train_x, train_y, validX, validY, epochnumber, lr
         save_weights_only=True,
         save_best_only=True,
     )
-
+    #train_x = np.concatenate((train_x, train_x), axis=2)
     input1 = Input(shape=(train_x.shape[1], train_x.shape[2]))
-    input2 = Input(shape=(train_x.shape[1], train_x.shape[2]))
 
     # Shared LSTM layers
-    lstm1 = LSTM(256, return_sequences=True)(input1)
+    lstm1 = LSTM(512, return_sequences=True)(input1)
     dropout1 = Dropout(0.3)(lstm1)
-    lstm2 = LSTM(128, return_sequences=True)(dropout1)
+    lstm2 = LSTM(256, return_sequences=True)(dropout1)
     dropout2 = Dropout(0.3)(lstm2)
-    lstm3 = LSTM(64, return_sequences=False)(dropout2)
+    lstm3 = LSTM(128, return_sequences=True)(dropout2)
     dropout3 = Dropout(0.3)(lstm3)
+    lstm4 = LSTM(64, return_sequences=False)(dropout3)
+    dropout4 = Dropout(0.3)(lstm4)
 
     # Output 1
-    output1 = Dense(1, activation="sigmoid")(dropout3)
+    output1 = Dense(2, activation="sigmoid")(dropout4)
 
     # Output 2 (conditional output)
-    output2 = Dense(1, activation="sigmoid")(dropout3)
-    output2 = tf.keras.layers.Multiply()([output1, output2])
 
     # Create the model
-    model = Model(inputs=[input1, input2], outputs=[output1, output2])
+    model = Model(inputs=[input1], outputs=[output1])
     model.summary()
 
     # Compile and train the model
-    model.compile(loss="binary_crossentropy", optimizer=opt, metrics=["accuracy",
-                                                                  tf.keras.metrics.Precision(name="precision"),
-                                                                  tf.keras.metrics.Recall(name="recall"),
-                                                                  tf.keras.metrics.AUC(name="auc"),
-                                                                  tfa.metrics.F1Score(num_classes=1,
-                                                                                      average='macro',
-                                                                                      threshold=0.5)
-                                                                  ])
-    history = model.fit([train_x, train_x], [train_y, train_y], epochs=epochnumber,
+    model.compile(loss="binary_crossentropy", optimizer=opt )
+
+    history = model.fit([train_x], [train_y], epochs=epochnumber,
                                                                 callbacks=[es_callback, reduce_lr, modelckpt_callback],
                                                                 verbose=1, shuffle=False,
-                                                                validation_data=([validX, validX], [validY, validY]))
+                                                                validation_data=([validX], [validY]))
 
     prediction = model.predict(validX)
     # Prediction and actual data plot
     plt.figure(figsize=(20, 6))
-    plt.plot(prediction[0:1440 * 3], label='prediction')
-    plt.plot(validY[0:1440 * 3], label='test_data')
+    plt.plot(prediction[0:1440 * 3,0], label='prediction')
+    plt.plot(validY[0:1440 * 3,0], label='test_data')
     plt.legend()
     plt.show()
 
 if __name__ == "__main__":
     #print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
-    train, test = loadeveryxml()
+    #train, test = loadeveryxml()
 
-    #train,patientdata=load(TRAIN2_540_PATH)
-    #test,patientdata=load(TEST2_540_PATH)
+    train,patientdata=load(TRAIN2_540_PATH)
+    test,patientdata=load(TEST2_540_PATH)
     train = data_preparation(train, pd.Timedelta(5, "m"), 30, 3)
     test = data_preparation(test, pd.Timedelta(5, "m"), 30, 3)
     model_base_RNN(dataTrain=train,dataValidation=test,lookback=50,maxfiltersize=10,epochnumber=200,modelnumber=2,learning_rate=0.001,oversampling=False)
