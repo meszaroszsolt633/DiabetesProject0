dc9f3792980fee406d43a923aef78e42ef116044
MarkFallen <erenyi.ate@gmail.com>
2023-09-26 20:09:33 +0200

    multioutput RNN now gives output (terrible)

--- modelMealClassificationRNN.py
+++ modelMealClassificationRNN.py
@@ -84,15 +84,152 @@ def show_plot(plot_data, delta, title):
     plt.show()
     return
 
-def create_dataset(dataset, look_back=1):
+
+
+#region MealDetectionModel
+def create_dataset_mealdetection(dataset, look_back=1):
+    dataX, dataY = [], []
+    for i in range(len(dataset) - look_back - 1):
+        a = dataset[i:(i + look_back), 1]
+        dataX.append(a)
+        dataY.append(dataset[i,0])
+    return np.array(dataX), np.array(dataY)
+def model_mealdetection_RNN(dataTrain, dataValidation, lookback=50, maxfiltersize=10, epochnumber=50, modelnumber=1, learning_rate=0.001, oversampling=False):
+    # TRAIN
+    feature_train1 = dataTrain['glucose_level']
+    feature_train1.loc[:, 'carbs'] = ""
+    feature_train1['carbs'] = feature_train1['carbs'].apply(lambda x: 0)
+
+    feature_train2 = dataTrain['meal']
+    feature_train2 = feature_train2.drop(['type'], axis=1)
+    feature_train2['carbs'] = feature_train2['carbs'].apply(lambda x: 1)
+
+    features_train = pd.concat([feature_train1, feature_train2])
+    features_train = features_train.sort_values(by='ts', ignore_index=True)
+
+    features_train_y = features_train['carbs']
+    features_train_y = ndimage.maximum_filter(features_train_y, size=maxfiltersize)
+    features_train_y = pd.DataFrame(features_train_y)
+
+    features_train_x = features_train['value']
+    features_train_x = pd.DataFrame(features_train_x)
+    features_train_x = features_train_x.fillna(method='ffill')
+    features_train_x['value'] = features_train_x['value'].astype('float64')
+
+
+
+    #VALIDATION
+
+    feature_validation1 = dataValidation['glucose_level']
+    feature_validation1.loc[:, 'carbs'] = ""
+    feature_validation1['carbs'] = feature_validation1['carbs'].apply(lambda x: 0)
+
+    feature_validation2 = dataValidation['meal']
+    feature_validation2 = feature_validation2.drop(['type'], axis=1)
+    feature_validation2['carbs'] = feature_validation2['carbs'].apply(lambda x: 1)
+
+    features_validation = pd.concat([feature_validation1, feature_validation2])
+    features_validation = features_validation.sort_values(by='ts', ignore_index=True)
+
+    features_validation_y = features_validation['carbs']
+    features_validation_y = ndimage.maximum_filter(features_validation_y, size=maxfiltersize)
+    features_validation_y = pd.DataFrame(features_validation_y)
+
+    features_validation_x = features_validation['value']
+    features_validation_x = pd.DataFrame(features_validation_x)
+    features_validation_x = features_validation_x.fillna(method='ffill')
+    features_validation_x['value'] = features_validation_x['value'].astype('float64')
+
+
+
+    featuresvalidation = pd.concat([features_validation_y, features_validation_x], axis=1)
+    featurestrain = pd.concat([features_train_y,features_train_x],axis=1)
+
+    featurestrain.columns = featurestrain.columns.astype(str)
+    featuresvalidation.columns = featuresvalidation.columns.astype(str)
+
+
+
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    featurestrain=scaler.fit_transform(featurestrain)
+    featuresvalidation=scaler.transform(featuresvalidation)
+    if(oversampling==True):
+        trainY=featurestrain[:,0]
+        trainX = featurestrain[:, 1]
+        trainY = trainY.reshape(-1, 1)
+        trainX = trainX.reshape(-1, 1)
+        smote = SMOTE(random_state=42)
+        trainX, trainY = smote.fit_resample(trainX, trainY)
+        featurestrain = np.column_stack((trainY, trainX))
+    trainX,trainY = create_dataset_mealdetection(featurestrain, lookback)
+    validX,validY = create_dataset_mealdetection(featuresvalidation, lookback)
+
+    trainX = np.reshape(trainX, (trainX.shape[0], trainX.shape[1], 1))
+    validX = np.reshape(validX, (validX.shape[0], validX.shape[1], 1))
+
+    print("trainX:",trainX.shape)
+    print("trainY:",trainY.shape)
+    print("validX:",validX.shape)
+    print("validY:", validY.shape)
+
+    model_meal_RNN(trainX, trainY, validX, validY, epochnumber, learning_rate)
+
+
+def model_meal_RNN(train_x, train_y, validX, validY,  epochnumber, lrnng_rate):
+    model = keras.Sequential()
+
+    opt = keras.optimizers.Adam(learning_rate=lrnng_rate)
+    path_checkpoint = "modelMealRNN_checkpoint.h5"
+    reduce_lr = keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=5, min_lr=0.001)
+    es_callback = keras.callbacks.EarlyStopping(monitor="val_loss", min_delta=0, patience=15)
+    modelckpt_callback = keras.callbacks.ModelCheckpoint(
+        monitor="val_loss",
+        filepath=path_checkpoint,
+        verbose=1,
+        save_weights_only=True,
+        save_best_only=True,
+    )
+
+    model.add(LSTM(256, return_sequences=True, input_shape=(train_x.shape[1], train_x.shape[2])))
+    model.add(Dropout(0.3))
+    model.add(LSTM(128, return_sequences=True))
+    model.add(Dropout(0.3))
+    model.add(LSTM(64, return_sequences=False))
+    model.add(Dropout(0.3))
+    model.add(Dense(1, activation="sigmoid"))
+
+    model.summary()
+    model.compile(loss="binary_crossentropy", optimizer=opt, metrics=["accuracy",
+                                                                      tf.keras.metrics.Precision(name="precision"),
+                                                                      tf.keras.metrics.Recall(name="recall"),
+                                                                      tf.keras.metrics.AUC(name="auc"),
+                                                                      tfa.metrics.F1Score(num_classes=1,
+                                                                                          average='macro',
+                                                                                          threshold=0.5)
+                                                                      ])
+    history = model.fit(train_x, train_y, epochs=epochnumber, callbacks=[es_callback, reduce_lr, modelckpt_callback],
+                        verbose=1, shuffle=False,
+                        validation_data=(validX, validY))
+
+    prediction = model.predict(validX)
+    # Prediction and actual data plot
+    plt.figure(figsize=(20, 6))
+    plt.plot(prediction[0:1440 * 3], label='prediction')
+    plt.plot(validY[0:1440 * 3], label='test_data')
+    plt.legend()
+    plt.show()
+
+#endregion
+
+#region MultiOutputModel
+def create_dataset_multi(dataset, look_back=1):
     dataX, dataY = [], []
     for i in range(len(dataset) - look_back - 1):
         a = dataset[i:(i + look_back), 2]
         dataX.append(a)
         dataY.append(dataset[i, 0:2])
     return np.array(dataX), np.array(dataY)
-
-def model_base_RNN(dataTrain, dataValidation, lookback=50, maxfiltersize=10, epochnumber=50, modelnumber=1, learning_rate=0.001, oversampling=False):
+def model_multi_RNN(dataTrain, dataValidation, lookback=50, maxfiltersize=10, epochnumber=50, modelnumber=1, learning_rate=0.001, oversampling=False):
     # TRAIN
 
     feature_train1 = dataTrain['glucose_level']
@@ -168,8 +305,8 @@ def model_base_RNN(dataTrain, dataValidation, lookback=50, maxfiltersize=10, epo
         smote = SMOTE(random_state=42)
         trainX, trainY = smote.fit_resample(trainX, trainY)
         featurestrain = np.column_stack((trainY, trainX))
-    trainX, trainY = create_dataset(featurestrain, lookback)
-    validX, validY = create_dataset(featuresvalidation, lookback)
+    trainX, trainY = create_dataset_multi(featurestrain, lookback)
+    validX, validY = create_dataset_multi(featuresvalidation, lookback)
 
     trainX = np.reshape(trainX, (trainX.shape[0], trainX.shape[1], 1))
     validX = np.reshape(validX, (validX.shape[0], validX.shape[1], 1))
@@ -180,52 +317,6 @@ def model_base_RNN(dataTrain, dataValidation, lookback=50, maxfiltersize=10, epo
     print("validY:", validY.shape)
 
     model_meal_RNN_multioutput(trainX, trainY, validX, validY,  epochnumber, learning_rate)
-
-
-def model_meal_RNN(train_x, train_y, validX, validY,  epochnumber, lrnng_rate):
-    model = keras.Sequential()
-
-    opt = keras.optimizers.Adam(learning_rate=lrnng_rate)
-    path_checkpoint = "modelMealRNN_checkpoint.h5"
-    reduce_lr = keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=5, min_lr=0.001)
-    es_callback = keras.callbacks.EarlyStopping(monitor="val_loss", min_delta=0, patience=15)
-    modelckpt_callback = keras.callbacks.ModelCheckpoint(
-        monitor="val_loss",
-        filepath=path_checkpoint,
-        verbose=1,
-        save_weights_only=True,
-        save_best_only=True,
-    )
-
-    model.add(LSTM(256, return_sequences=True, input_shape=(train_x.shape[1], train_x.shape[2])))
-    model.add(Dropout(0.3))
-    model.add(LSTM(128, return_sequences=True))
-    model.add(Dropout(0.3))
-    model.add(LSTM(64, return_sequences=False))
-    model.add(Dropout(0.3))
-    model.add(Dense(1, activation="sigmoid"))
-
-    model.summary()
-    model.compile(loss="binary_crossentropy", optimizer=opt, metrics=["accuracy",
-                                                                      tf.keras.metrics.Precision(name="precision"),
-                                                                      tf.keras.metrics.Recall(name="recall"),
-                                                                      tf.keras.metrics.AUC(name="auc"),
-                                                                      tfa.metrics.F1Score(num_classes=1,
-                                                                                          average='macro',
-                                                                                          threshold=0.5)
-                                                                      ])
-    history = model.fit(train_x, train_y, epochs=epochnumber, callbacks=[es_callback, reduce_lr, modelckpt_callback],
-                        verbose=1, shuffle=False,
-                        validation_data=(validX, validY))
-
-    prediction = model.predict(validX)
-    # Prediction and actual data plot
-    plt.figure(figsize=(20, 6))
-    plt.plot(prediction[0:1440 * 3], label='prediction')
-    plt.plot(validY[0:1440 * 3], label='test_data')
-    plt.legend()
-    plt.show()
-
 def model_meal_RNN_multioutput(train_x, train_y, validX, validY, epochnumber, lrnng_rate):
     opt = keras.optimizers.Adam(learning_rate=lrnng_rate)
     path_checkpoint = "modelMealRNN_checkpoint.h5"
@@ -276,6 +367,8 @@ def model_meal_RNN_multioutput(train_x, train_y, validX, validY, epochnumber, lr
     plt.legend()
     plt.show()
 
+#endregion
+
 if __name__ == "__main__":
     #print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
     #train, test = loadeveryxml()
@@ -284,4 +377,4 @@ if __name__ == "__main__":
     test,patientdata=load(TEST2_540_PATH)
     train = data_preparation(train, pd.Timedelta(5, "m"), 30, 3)
     test = data_preparation(test, pd.Timedelta(5, "m"), 30, 3)
-    model_base_RNN(dataTrain=train,dataValidation=test,lookback=50,maxfiltersize=10,epochnumber=200,modelnumber=2,learning_rate=0.001,oversampling=False)
+    model_mealdetection_RNN(dataTrain=train,dataValidation=test,lookback=50,maxfiltersize=10,epochnumber=200,modelnumber=2,learning_rate=0.001,oversampling=False)
